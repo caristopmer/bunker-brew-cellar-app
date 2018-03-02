422a4fad56dce1090e01d7ab12c4b6b6d8e1a523
Chris McMenomy <chris.mcmenomy@gmail.com>
2018-03-01 20:56:27 -0600

    home index page displays beers pulled from back end api.

--- src/app/beer-index/beer-index.component.ts
+++ src/app/beer-index/beer-index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Beer } from '../beer';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-beer-index',
@@ -7,33 +8,55 @@ import { Beer } from '../beer';
   styleUrls: ['./beer-index.component.css']
 })
 export class BeerIndexComponent implements OnInit {
-  beers: Array<any>;
+  beers: Array<any> = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
-    this.beers = [
-      {
-        name: 'this beer',
-        quantity: 6,
-        brewery: 'Dogfish Head'
-      },
-      {
-        name: 'that beer',
-        quantity: 3,
-        brewery: 'Yo dawg brewing co.'
-      },
-      {
-        name: 'another beer',
-        quantity: 2,
-        brewery: 'Miller Brewing Company'
-      },
-      {
-        name: 'here is a different beer',
-        quantity: 9,
-        brewery: 'Three Floyds Brewing Company'
-      },
-    ];
+    const beerIndexUrl = 'http://localhost:3000/beers';
+
+    this.http.get(beerIndexUrl)
+      .subscribe(response => {
+        console.log(response);
+        for (let i = 0; i < response.length; i++) {
+          this.beers.push(
+            {
+              name: response[i].name,
+              quantity: response[i].quantity,
+              brewery: response[i].brewery_id
+            }
+          );
+        }
+      });
+
+
+
+
+
+    // this.beers = [
+    //   {
+    //     name: 'this beer',
+    //     quantity: 6,
+    //     brewery: 'Dogfish Head'
+    //   },
+    //   {
+    //     name: 'that beer',
+    //     quantity: 3,
+    //     brewery: 'Yo dawg brewing co.'
+    //   },
+    //   {
+    //     name: 'another beer',
+    //     quantity: 2,
+    //     brewery: 'Miller Brewing Company'
+    //   },
+    //   {
+    //     name: 'here is a different beer',
+    //     quantity: 9,
+    //     brewery: 'Three Floyds Brewing Company'
+    //   },
+    // ];
   }
 
 }
