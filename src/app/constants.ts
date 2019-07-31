7982d9c8db03f3b50473a3eb57b0cdb56cf517b2
Chris McMenomy <chris.mcmenomy@gmail.com>
2019-07-30 19:01:55 -0500

    updating BE url to actual

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,7 +1,7 @@
 
-// export const BACKENDAPIURL = 'https://bunker-brew-cellar-api.herokuapp.com/';
+export const BACKENDAPIURL = 'https://bunker-brew-cellar-api.herokuapp.com/';
 
-export const BACKENDAPIURL = 'http://localhost:3000/';
+// export const BACKENDAPIURL = 'http://localhost:3000/';
 
 export const routeConstants = {
   homeRoute: 'Home'
